41e32852633803087985e0f1658bbd2697500c6d
eduardocaas <eduardofc.dev@gmail.com>
2023-06-30 18:45:38 -0300

    Entities and config imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { ProjectModule } from './entities/project/project.module';
+import { TaskModule } from './entities/task/task.module';
+import { UserModule } from './entities/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProjectModule, TaskModule, UserModule],
   controllers: [],
   providers: [],
 })
