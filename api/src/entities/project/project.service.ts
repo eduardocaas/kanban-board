efa70216fa6a8d231d1d55a47396bb0b5943f906
eduardocaas <eduardofc.dev@gmail.com>
2023-07-03 22:40:58 -0300

    Method save with validations for Project

---
+++ api/src/entities/project/project.service.ts
@@ -0,0 +1,30 @@
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { ProjectRepository } from './project.repository';
+import { FindOneOptions } from 'typeorm';
+
+@Injectable()
+export class ProjectService {
+
+  constructor(@InjectRepository(Project) private readonly projectRepository: ProjectRepository) {}
+
+  async save(project: Project): Promise<Project> {
+
+    if(!project.title || project.title.trim().length === 0) {
+      throw new BadRequestException('Project title must not be blank');
+    }
+
+    if(project.title.length < 4) {
+      throw new BadRequestException('Title must have at least 4 characters');
+    }
+
+    const options: FindOneOptions = { where: {title : project.title}};
+    const obj = await this.projectRepository.findOne(options);
+    if(obj) {
+      throw new ConflictException('Title is already in use');
+    }
+
+    return this.projectRepository.save(project);
+  }
+}
