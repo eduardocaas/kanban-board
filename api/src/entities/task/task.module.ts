10146b8f85e2f35f9e8899a48e733ef65ca294d1
eduardocaas <eduardofc.dev@gmail.com>
2023-07-04 20:04:58 -0300

    Add imports

--- api/src/entities/task/task.module.ts
+++ api/src/entities/task/task.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
+import { Task } from './task.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Task, TaskRepository])],
+  controllers: [TaskController],
+  providers: [TaskService, TaskRepository]
+})
 export class TaskModule {}
