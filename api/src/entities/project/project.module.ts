88b97f77cc98b0f8b5465ffac7f5a5ce062cb4fd
eduardocaas <eduardofc.dev@gmail.com>
2023-07-03 22:43:52 -0300

    Module for Project

--- api/src/entities/project/project.module.ts
+++ api/src/entities/project/project.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ProjectController } from './project.controller';
+import { ProjectService } from './project.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { ProjectRepository } from './project.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Project, ProjectRepository])],
+  controllers: [ProjectController],
+  providers: [ProjectService, ProjectRepository]
+})
 export class ProjectModule {}
