d072a7d8277a1ec15f05b5055137efccc3ae3845
eduardocaas <eduardofc.dev@gmail.com>
2023-07-03 22:44:10 -0300

    Module for User

--- api/src/entities/user/user.module.ts
+++ api/src/entities/user/user.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
+  controllers: [UserController],
+  providers: [UserService, UserRepository]
+})
 export class UserModule {}
