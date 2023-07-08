c83cddfac0de794b09c64097816eab37c1e95b3e
eduardocaas <eduardofc.dev@gmail.com>
2023-07-07 23:29:53 -0300

    Update method

--- api/src/entities/task/task.controller.ts
+++ api/src/entities/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res, BadRequestException, NotFoundException, ConflictException } from '@nestjs/common';
+import { Body, Controller, Post, Res, BadRequestException, NotFoundException, ConflictException, Put, InternalServerErrorException } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './task.entity';
 import { Response } from 'express';
@@ -33,6 +33,37 @@ export class TaskController {
       }
       else if (error instanceof NotFoundException) {
         statusCode = 404;
+        errorMessage = error.message;
+      }
+
+      res.status(statusCode).send({ message: errorMessage, timestamp });
+    }
+  }
+
+  @Put()
+  async update(@Body() task: Task, @Res() res: Response): Promise<void> {
+    try {
+      const obj = await this.taskService.update(task);
+      res.status(200).send(obj);
+    } 
+    catch (error) {
+      let statusCode = 500;
+      let errorMessage = 'Server error updating task';
+      let timestamp = new Date().toISOString();
+
+      if (!(error instanceof InternalServerErrorException)) {
+        if (error instanceof BadRequestException) {
+          statusCode = 400;
+        } 
+        else if (error instanceof ConflictException) {
+          statusCode = 409;
+        }
+        else if (error instanceof NotFoundException) {
+          statusCode = 404;
+        } 
+        else if (error instanceof InternalServerErrorException) {
+          statusCode = 500;
+        }
         errorMessage = error.message;
       }
 
