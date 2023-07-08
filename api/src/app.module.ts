d1d8b2a009fdc33118e45cba33393ebc7dc1a53b
eduardocaas <eduardofc.dev@gmail.com>
2023-07-07 23:29:08 -0300

    Refactor imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,14 @@ import { typeOrmConfig } from './configs/typeorm.config';
 import { ProjectModule } from './entities/project/project.module';
 import { TaskModule } from './entities/task/task.module';
 import { UserModule } from './entities/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProjectModule, TaskModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig), 
+    ProjectModule, TaskModule, UserModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [],
   providers: [],
 })
