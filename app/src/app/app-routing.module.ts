1bd772f1a73a4f57e836ea84611199f3f457b704
eduardocaas <eduardofc.dev@gmail.com>
2023-07-09 20:51:26 -0300

    Default routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { KanbanComponent } from './components/kanban/kanban.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent, children: [
+      {path: 'kanban/:id', component: KanbanComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
