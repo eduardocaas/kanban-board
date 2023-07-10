ca9d2b2bd22c16e96de489989d3d1d2649a69b37
eduardocaas <eduardofc.dev@gmail.com>
2023-07-09 23:51:05 -0300

    Organize imports

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,21 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { KanbanComponent } from './components/kanban/kanban.component';
+import { NavComponent } from './components/nav/nav.component';
+
 import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    KanbanComponent
+    KanbanComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
