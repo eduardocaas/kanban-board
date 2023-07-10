f4f3e612ade7fa7e647873d266d5541c4a9c6784
eduardocaas <eduardofc.dev@gmail.com>
2023-07-09 23:50:55 -0300

    Fix routing model

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { KanbanComponent } from './components/kanban/kanban.component';
+import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: '', component: DashboardComponent, children: [
+    path: '', component: NavComponent, children: [
+      {path: 'dashboard', component: DashboardComponent},
       {path: 'kanban/:id', component: KanbanComponent}
     ]
   }
