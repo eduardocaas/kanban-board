3bc15ade3af6bfae51dea08d8be1d1bcf4395dac
eduardocaas <eduardofc.dev@gmail.com>
2023-07-10 23:15:52 -0300

    Service with CRUD

--- app/src/app/service/task.service.ts
+++ app/src/app/service/task.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task.model';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  save(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${API_CONFIG.baseUrl}/task`, task);
+  }
+
+  findByProjectId(id: number): Observable<Task[]> {
+    return this.http.get<Task[]>(`${API_CONFIG.baseUrl}/task/${id}`);
+  }
+
+  findById(id: number): Observable<Task> {
+    return this.http.get<Task>(`${API_CONFIG.baseUrl}/task/get/${id}`);
+  }
+
+  update(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${API_CONFIG.baseUrl}/task`, task);
+  }
+
+  delete(id: number): Observable<Task> {
+    return this.http.delete<Task>(`${API_CONFIG.baseUrl}/task/${id}`);
+  }
+
 }
