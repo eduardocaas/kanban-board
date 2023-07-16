5a7e17db8073474e749fd9278c781c790d3a30a4
eduardocaas <eduardofc.dev@gmail.com>
2023-07-15 23:22:54 -0300

    Delete method

--- app/src/app/service/project.service.ts
+++ app/src/app/service/project.service.ts
@@ -11,11 +11,15 @@ export class ProjectService {
 
   constructor(private http: HttpClient) { }
 
-  save(project: Project): Observable<Project> {
+  save(project: Project): Observable<any> {
     return this.http.post<Project>(`${API_CONFIG.baseUrl}/project`, project);
   }
 
   findAll(): Observable<Project[]> {
     return this.http.get<Project[]>(`${API_CONFIG.baseUrl}/project`);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${API_CONFIG.baseUrl}/project/${id}`);
   }
 }
